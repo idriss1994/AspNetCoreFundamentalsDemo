a87a557cf0f0a29282c79066e3bb383334992140
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-22 00:02:00 +0100

    Changeing the implementation of IMyService with the concrete type MyService2 without makeing changes to IndexModel class that dends to IMyService service

---
+++ AspNetCoreFundamentalsDemo/MyDependency2.cs
@@ -0,0 +1,16 @@
+﻿namespace AspNetCoreFundamentalsDemo
+{
+    public class MyDependency2 : IMyDependency
+    {
+        private readonly ILogger<MyDependency2> _logger;
+
+        public MyDependency2(ILogger<MyDependency2> logger)
+        {
+            _logger = logger;
+        }
+        public void WriteMessage(string message)
+        {
+            _logger.LogInformation($"MyDependency2.WriteMessage Message: {message}");
+        }
+    }
+}
--- AspNetCoreFundamentalsDemo/Program.cs
+++ AspNetCoreFundamentalsDemo/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddScoped<IMyDependency, MyDependency>();
+builder.Services.AddScoped<IMyDependency, MyDependency2>();
 
 var app = builder.Build();
 
